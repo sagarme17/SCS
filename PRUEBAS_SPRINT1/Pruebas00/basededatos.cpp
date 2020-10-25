ddf2a9adb6182de47554b91ed469478af8f7a7de
Rocio <95barojas@gmail.com>
2020-10-25 00:11:47 -0500

    Pruebas Sprint 2

--- PRUEBAS_SPRINT1/Pruebas00/basededatos.cpp
+++ PRUEBAS_SPRINT1/Pruebas00/basededatos.cpp
@@ -133,4 +133,151 @@ bool baseDatos::InsertarFoto1(QString direccion,int matricula)
     }
 
 }
+/*---Segundo Sprint---*/
+bool baseDatos::generarPDF(int matricula)
+{
+    QSqlQuery acceso(mDatabase),query1(mDatabase),query2(mDatabase),query3(mDatabase);
+    QString matriculaA=QString::number(matricula),cod,totalN,totalPeriodo;
+    int total=0,cred,hora,totalH=0;
+    acceso.prepare("Select * from alumno where Matricula='"+matriculaA+"'");
+    acceso.exec();
+    if(acceso.next())
+      {
+        qDebug()<<"Matricula Existente";
+        query1.prepare("select Código from alumno inner join cursando on alumno.Matricula=cursando.Matricula where alumno.Matricula='"+matriculaA+"'");
+        query1.exec();
+        qDebug()<<"------Materias cursando------";
+        while (query1.next()) {
+            cod=query1.value(0).toString();
+            qDebug()<<"Código_Materia es: "+cod;
+            query2.prepare("select Creditos from materia where Código='"+cod+"'");
+            query2.exec();
+            query3.prepare("select Horas_PeriodoPT from materia where Código='"+cod+"'");
+            query3.exec();
+           if(query2.next() and query3.next())
+            {
+                cred=query2.value(0).toInt();
+                hora=query3.value(0).toInt();
+                qDebug()<<"Creditos: "+QString::number(cred);
+                qDebug()<<"Horas_Periodo : "+QString::number(hora);
+                total+=cred;
+                totalH+=hora;
+            }
 
+        }
+        totalN=QString::number(total);
+        qDebug()<<"El total de creditos cargados son: "+totalN;
+        qDebug()<<"El total de horas(Teoria y practica) cargados son: "+QString::number(totalH);
+        return true;
+    }else
+       {
+            qDebug()<<"Matricula NO Existente";
+            return false;
+        }
+
+}
+
+bool baseDatos::HacerSolicitud(int matricula)
+{
+    QSqlQuery registro(mDatabase);
+    QString matriculaA=QString::number(matricula);
+    if(registro.exec("INSERT INTO `solicitud` ( `Matricula`) VALUES ('"+matriculaA+"')"))
+    {
+        qDebug()<<"Solicitud Cargada al alumno: "+matriculaA;
+        return true;
+    }else
+    {
+        qDebug()<<"No se pudo realizar la solicitud a la alumno: "+ matriculaA;
+        return false;
+    }
+}
+bool baseDatos::BuscarSolicitud(int matricula)
+{
+    QSqlQuery verSolicitud(mDatabase);
+    verSolicitud.prepare("select *from alumno inner join solicitud where solicitud.Matricula='"+QString::number(matricula)+"'");
+    verSolicitud.exec();
+    if(verSolicitud.next())
+    {
+        qDebug()<<"Solicitud Encontrada!!!";
+        return true;
+    }else
+    {
+        return false;
+    }
+}
+bool baseDatos::RestablecerContrasena(int matricula)
+{
+    QSqlQuery solicitud(mDatabase),query1(mDatabase),query2(mDatabase);
+    QString matriculaA=QString::number(matricula);
+    solicitud.prepare("select * from solicitud where solicitud.Matricula='"+matriculaA+"'");
+    solicitud.exec();
+    if(solicitud.next())
+      {
+        qDebug()<<"Alumno CON solicitud";
+
+        if(query1.exec("update usuario set usuario.Contraseña='"+matriculaA+"'where   usuario.Id_Usuario='"+matriculaA+"'"))
+        {
+            qDebug()<<"Se restablecio la contraseña del alumno: "+matriculaA;
+            query2.prepare("DELETE FROM solicitud WHERE solicitud.Matricula = '"+matriculaA+"'");
+            query2.exec();
+            query2.next();
+            qDebug()<<"Solicitud asociada con la matricula "+matriculaA+" eliminada";
+            return true;
+        }else
+        {
+            qDebug()<<"No se pudo actualizar";
+            return false;
+        }
+      }else
+       {
+          qDebug()<<"Alumno SIN solicitud";
+          return false;
+       }
+
+
+}
+bool baseDatos::MarcarMateriasPorCursar(int matricula, QString codigo)
+{
+    QSqlQuery query1(mDatabase),query2(mDatabase),registro(mDatabase),query3(mDatabase);
+    QString matriculaA=QString::number(matricula),pre;
+    /*Verifico se la materia que quiero marcar tiene prerequisito*/
+    query1.prepare("select CódigoPre from materia inner join prerequisito on materia.Código=prerequisito.Código where prerequisito.Código='"+codigo+"'");
+    query1.exec();
+    if(query1.next()) {
+        pre=query1.value(0).toString();
+        qDebug()<<"Materia con prerequisito "+pre;
+        /*Verifico que esta como prerequisito ya la aprobe*/
+        query2.prepare("select Código from alumno inner join aprobado on alumno.Matricula=aprobado.Matricula where aprobado.Matricula='"+matriculaA+"' and aprobado.Código='"+pre+"'");
+        query2.exec();
+        if(query2.next())
+        {
+            qDebug()<<"La materia prerequisito "+pre+" esta aprobada";
+            /*Cargar materia a tabla de porcursar*/
+            if(registro.exec("INSERT INTO porcursar (`Matricula`, `Código`) VALUES ('"+matriculaA+"', '"+codigo+"')"))
+            {
+                qDebug()<<"Materia "+codigo+ "marcada como PorCursar";
+                return true;
+            }
+            else
+            {
+                qDebug()<<"Materia "+codigo+ "SIN marcar como PorCursar";
+                return false;
+            }
+        }
+        else
+        {
+           qDebug()<<"La materia con código "+pre+" no esta aprobada";
+           return false;
+        }
+    }else
+    {
+        qDebug()<<"Materia SIN prerequisito ";
+        qDebug()<<"Se restablecio la contraseña del alumno: "+matriculaA;
+        query2.prepare("INSERT INTO porcursar (`Matricula`, `Código`) VALUES ('"+matriculaA+"', '"+codigo+"')");
+        query2.exec();
+        query2.next();
+        qDebug()<<"Se ha cargado la materia "+codigo+" al alumno "+matriculaA;
+        return false;
+    }
+
+}
--- PRUEBAS_SPRINT1/Pruebas00/tst_pruebas0.cpp
+++ PRUEBAS_SPRINT1/Pruebas00/tst_pruebas0.cpp
@@ -25,6 +25,11 @@ private slots:
     void CambiarContraAlumno();
     void GeneralAutenticarse();
     void Foto();
+    void GenerarEst();
+    void Solicitud_p1();
+    void Solicitud_p2();
+    void RestablecerContra();
+    void MateriasPorCursar();
 
 
 };
@@ -42,7 +47,7 @@ void Pruebas0::conexionBase()
 }
 void Pruebas0::AutenticarseAdmin()
 {
-    QCOMPARE(conexion->AutenticarseAdmin1(),true);
+    //QCOMPARE(conexion->AutenticarseAdmin1(),true);
 }
 void Pruebas0::AutenticarseAlumno1()
 {
@@ -58,14 +63,32 @@ void Pruebas0::CambiarContraAlumno()
 }
 void Pruebas0::GeneralAutenticarse()
 {
-    QCOMPARE(conexion->GeneralAutenticarse(201617602,201617602),true);
+    //QCOMPARE(conexion->GeneralAutenticarse(201617602,201617602),true);
 }
 void Pruebas0::Foto()
 {
-    QVERIFY(conexion->InsertarFoto1("D:/Descargas/panaMiguel.jpg",201617587));
+   // QVERIFY(conexion->InsertarFoto1("D:/Descargas/panaMiguel.jpg",201617587));
 }
-
-
+void Pruebas0::GenerarEst()
+{
+    QCOMPARE(conexion->generarPDF(201772826),true);
+}
+void Pruebas0::Solicitud_p1()
+{
+    QVERIFY(conexion->HacerSolicitud(201618233));
+}
+void Pruebas0::Solicitud_p2()
+{
+     QCOMPARE(conexion->BuscarSolicitud(201624489),true);
+}
+void Pruebas0::RestablecerContra()
+{
+    QVERIFY(conexion->RestablecerContrasena(201617942));
+}
+void Pruebas0::MateriasPorCursar()
+{
+    QCOMPARE(conexion->MarcarMateriasPorCursar(201617942,"CCOS 007"),true);
+}
 QTEST_APPLESS_MAIN(Pruebas0)
 
 #include "tst_pruebas0.moc"
