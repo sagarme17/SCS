0e5c77a6660882e05dafc3ba3905ed7a89e4195a
Saul Garcia <sagarme.17g@gmail.com>
2020-10-28 21:13:23 -0600

    ARREGLOS DE SOSA

--- SCS/reestablecer_contrasenia.cpp
+++ SCS/reestablecer_contrasenia.cpp
@@ -1,15 +1,20 @@
 #include "reestablecer_contrasenia.h"
 #include "ui_reestablecer_contrasenia.h"
-#include "restablecer_contrasena1.h"
-#include "ui_restablecer_contrasena1.h"
+#include "administrador.h"
+#include "alumno.h"
+#include <QDebug>
+#include <QMessageBox>
+#include <QWidget>
+#include <QtSql/QSqlDatabase>
 
 reestablecer_Contrasenia::reestablecer_Contrasenia(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::reestablecer_Contrasenia)
+
 {
+    this->mdb=QSqlDatabase::database("Connection");
     ui->setupUi(this);
 }
-
 reestablecer_Contrasenia::~reestablecer_Contrasenia()
 {
     delete ui;
@@ -17,9 +22,13 @@ reestablecer_Contrasenia::~reestablecer_Contrasenia()
 
 void reestablecer_Contrasenia::on_Reestablecer_clicked()
 {
-   restablecer_contrasena1 Contra(this);
-    Contra.setWindowTitle("Recuperar Contraseña");
-    Contra.exec();
+    QString matri;
+    matri=ui->Matricula->text();
+    QSqlQuery query1(mdb);
+    query1.prepare("insert into solicitud(matricula) value('"+matri+"')");
+    query1.exec();
+    query1.next();
+    qDebug()<<matri;
 }
 
 void reestablecer_Contrasenia::on_Regresar_2_clicked()
