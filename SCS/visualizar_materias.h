61903ebb0f21a318db0d09f289779c63c3d885e1
Esteban <estebanblade1@gmail.com>
2020-11-07 20:12:24 -0600

    Visualizar materias y modificación en alumno.

---
+++ SCS/visualizar_materias.h
@@ -0,0 +1,30 @@
+#ifndef VISUALIZAR_MATERIAS_H
+#define VISUALIZAR_MATERIAS_H
+
+#include <QDialog>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDialog>
+
+namespace Ui {
+class visualizar_materias;
+}
+
+class visualizar_materias : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit visualizar_materias(QString matricula,QWidget *parent = nullptr);
+    ~visualizar_materias();
+
+private slots:
+    void on_Regresar_clicked();
+
+private:
+    Ui::visualizar_materias *ui;
+    QString matricula;
+    QSqlDatabase mdb;
+};
+
+#endif // VISUALIZAR_MATERIAS_H
